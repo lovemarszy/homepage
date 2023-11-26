65951526c24255d4c8f89b06208dfc5dd0717a72
Marszy <fzyuan0529@gmail.com>
2023-11-26 21:23:02 +0800

    提交信息

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,15 +2,19 @@ const Footer = () => {
   return (
     <footer className="mb-4 mt-10 text-center text-sm text-zinc-300/80 md:my-8">
       <div className="mb-1">
-        Modified from ⁣⁣⁣⁣{" "}
-        <a href="https://github.com/aozaki-kuro/aozaki.cc" target="_blank" rel="noopener noreferrer">
+        Modified from ⁣⁣⁣⁣{' '}
+        <a
+          href="https://github.com/aozaki-kuro/aozaki.cc"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Aozaki
-        </a>{" "}
+        </a>{' '}
       </div>
-      <div className="mb-1">Built with love</div>
+      <div className="mb-1">Built by Marszy</div>
       <div>©️ 2023 - {new Date().getFullYear()}</div>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
